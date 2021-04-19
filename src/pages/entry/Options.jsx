77a96fec026b4b3be012b7be820cc5784e7baf10
iamtreetop <trivta@gmail.com>
2021-04-19 11:20:30 -0700

    Test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,9 +4,11 @@ import Row from "react-bootstrap/Row";
 
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionTYpe is 'scoops' or 'toppings'
   useEffect(() => {
@@ -14,9 +16,13 @@ const Options = ({ optionType }) => {
       .get(`http://localhost:3030/${optionType}`)
       .then((res) => setItems(res.data))
       .catch((err) => {
-        console.log(err);
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: replace null with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
