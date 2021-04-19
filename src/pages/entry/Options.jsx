f2c55945a89050c4fdfd2b4da1d7d7a4abc926e2
iamtreetop <trivta@gmail.com>
2021-04-18 22:50:43 -0700

    Add ScoopsOption component to app, Wrote test to check for scoop images

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+
+import ScoopOption from "./ScoopsOption";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  // optionTYpe is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [optionType]);
+
+  // TODO: replace null with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
