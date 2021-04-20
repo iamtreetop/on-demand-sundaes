b4af8a85b3fa89446bdcfb9c99f48e47979afb7a
iamtreetop <trivta@gmail.com>
2021-04-20 15:41:23 -0700

    Add initial state for grand total in Orderentry to fix unmounted compoennt error

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -52,19 +52,13 @@ test("Update toppings subtotal when toppings change", async () => {
 });
 
 describe("Grand total", () => {
-  test("grand total starts at $0.00", () => {
-    render(<OrderEntry />);
-    const grandTotal = screen.getByRole("heading", {
-      name: /grand total: \$/i,
-    });
-    expect(grandTotal).toHaveTextContent("0.00");
-  });
-
   test("grand total updates properly if scoop is added first", async () => {
     render(<OrderEntry />);
     const grandTotal = screen.getByRole("heading", {
       name: /grand total: \$/i,
     });
+    // check that the grand total starts at 0.00
+    expect(grandTotal).toHaveTextContent("0.00");
 
     // update vanilla scoops to 2 and check grand total
     const vanillaInput = await screen.findByRole("spinbutton", {
