9df58eff3b4567a05551cf19a2e7b32c5d941727
iamtreetop <trivta@gmail.com>
2021-04-21 23:26:18 -0700

    Add and code to test for disabled order button in orderEntry if no scoops are ordered

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -3,6 +3,7 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -24,4 +25,25 @@ test("Handles errors for scoops and toppings routes", async () => {
     const alerts = await screen.findByRole("alert");
     expect(alerts).toBeInTheDocument(2);
   });
+});
+
+test("Order button is enabled only when scoops are ordered", async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  // order button disabled as default
+  let orderButton = screen.getByRole("button", { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // enter scoop, order button enabled
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  // clear scoops, disable order button
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
